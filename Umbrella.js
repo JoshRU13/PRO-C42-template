e5d1393e0e1e5064abab4c6ff881e0c4e60e4ad1
JoshRU13 <69476986+JoshRU13@users.noreply.github.com>
2021-05-05 17:44:11 -0600

    Update Umbrella.js

--- Umbrella.js
+++ Umbrella.js
@@ -8,13 +8,16 @@ class Umbrella {
         this.radius = 50;
         World.add(world, this.umbrella)
         //load Image for BestMan
-        
+        this.image2 = loadImage("`Walking Frame/batman.png")
     }
 
     display(){
         var pos = this.umbrella.position;
         imageMode(CENTER);
         //display the image for BestMan if the frameCount is over 200, otherwise display the boy with umbrella image
+        if(frameCount>=200)
+            image(this.image2,pos.x,pos.y+80,150,230)
+        else
         image(this.image,pos.x,pos.y+70,300,300);
     }
 }
